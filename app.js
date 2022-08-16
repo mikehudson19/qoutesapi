cd85cd6fb43968eb30f46aaf63e803a95acefe2c
Michael Hudson <mikehudson19@live.co.uk>
2022-08-16 21:43:02 +0200

    add another route

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.get("/", (req, res) => {
     res.send("You have hit the right route");
 });
 
+app.get("/test", (req, res) => {
+    res.send("The API is running!");
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running on PORT ${port}`)
